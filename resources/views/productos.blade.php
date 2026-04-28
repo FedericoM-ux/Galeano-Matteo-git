96ee8123874fce4c8f2258ad0fde9e56bd16d9c4
Fede <federicoomatteo@gmail.com>
2026-04-28 20:14:30 -0300

    Se mejoro el codigo para que sea mas reponsive, incluyendo un menú para moviles y un cambio en la sección de copyright para que funcione correctamente en este último

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -2,59 +2,76 @@
 <html>
 
 <head>
-    <title>Productos</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Dejada Padel Store / Productos</title>
+    <link rel="icon" type="image/png" href="images/pelota.png">
     <link rel="stylesheet" href="{{ asset('css/estilo.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
 </head>
-<nav class="navbar navbar-expand-lg navbar-light navbar">
-    <div class="container-fluid py-3">
-            <div class="col-md-3">
-                <a class="navbar-brand d-flex align-items-center" href="/main">
-                    <img src="{{ asset('images/Icono.PNG') }}" alt="Logo La Dejada" width="180" class="me-2">
-                </a>
-            </div>
-            
-            <div class="col-md-6">
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Buscar productos..." aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-            </div>
-            
-            <a class="navbar-brand ms-auto" href="/user">
-                <img src="{{ asset('images/user.svg') }}" height="30">
-            </a>
-                    <a class="navbar-brand position-relative" href="/shop">
-    <img src="{{ asset('images/shopping-cart.svg') }}" height="30">
-    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
-</a>
-            </div>
+
+<body>
+<nav class="navbar navbar-dark navbar">
+    <div class="container-fluid">
+
+    <div class="row w-100 align-items-center">
+
+    <div class="col-12 col-md-3 text-center text-md-start mb-2 mb-md-0">
+        <a href="/main">
+            <img src="{{ asset('images/Icono.PNG') }}" class="img-fluid" style="max-height:60px;">
+        </a>
+    </div>
+
+    <div class="col-12 col-md-6 mb-2 mb-md-0">
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Buscar productos...">
+            <button class="btn btn-outline-light">Buscar</button>
+        </form>
+    </div>
+
+    <div class="col-12 col-md-3 d-flex justify-content-center justify-content-md-end gap-3">
+    
+        <a href="/user">
+            <img src="{{ asset('images/user.svg') }}" height="28">
+        </a>
+
+        <a href="/shop" class="position-relative">
+            <img src="{{ asset('images/shopping-cart.svg') }}" height="28">
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
+        </a>
+        </div>
+        </div>
     </div>
 </nav>
 
-    <nav class="navbar navbar-expand-lg navbar-light navbarW">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto text-uppercase fw-semibold">
-<li class="nav-item"><a class="nav-link px-3" href="/main">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link px-3" href="/productos">Productos</a></li>
-                    <li class="nav-item"><a class="nav-link px-3" href="/ofertas">Ofertas</a></li>
-                    <li class="nav-item"><a class="nav-link px-3" href="/ventaMayorista">Venta Mayorista</a></li>
-                    <li class="nav-item"><a class="nav-link px-3" href="/sobre-nosotros">Sobre Nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link px-3" href="/comercialización">Comercialización</a></li>
-                    <li class="nav-item"><a class="nav-link px-3" href="/contacto">Contáctanos</a>
-                    <li class="nav-item"><a class="nav-link px-3" href="/términos">Términos y Usos</a></li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-light navbarW">
+    <div class="container">
+
+        <input type="checkbox" id="menu-toggle">
+
+        <label for="menu-toggle" class="navbar-toggler">
+        <img src="{{ asset('images/menu.svg') }}" width="40" alt="Menú">
+        </label>
+
+        <div class="menu-mobile">
+        <ul class="navbar-nav mx-auto text-uppercase fw-semibold">
+
+        <li class="nav-item"><a class="nav-link px-3" href="/main">Inicio</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/productos">Productos</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/ofertas">Ofertas</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/ventaMayorista">Venta Mayorista</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/sobre-nosotros">Sobre Nosotros</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/comercialización">Comercialización</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/contacto">Contáctanos</a></li>
+        <li class="nav-item"><a class="nav-link px-3" href="/términos">Términos y Usos</a></li>
+        </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
 <div class="separadorG"></div>
 
-<div class = "container my-5">
+<div class = "container-fluid my-5">
     <div class = "row g-4">
         <div class = "col-12 col-md-6 col-lg-3">
             <div class = "card h-100 border-0 shadow-sm text-center">
@@ -168,14 +185,20 @@
 
 <div class="linea-end"></div>
 
-<nav class="navbar navbar-expand-lg navbar-dark navbarW">
-    <a class="navbar-brand mx-auto">
-        <p class="text-dark"><br>Seguinos en nuestras redes sociales: </p>
-        <img src="{{ asset('images/brand-whatsapp.svg') }}" alt="wpp" width="40">
-        <img src="{{ asset('images/brand-facebook.svg') }}" alt="fb" width="40">
-        <img src="{{ asset('images/brand-instagram.svg') }}" alt="ig" width="40">
-        <p class="text-dark"><br>© 2026 - La Dejada Padel - Todas las marcas son propiedad de sus respectivos dueños - Todos los derechos reservados</p>
-    </a>
+<nav class="navbar navbar-dark navbarW">
+<div class="container text-center py-4">
+
+<p class="mb-3 text-dark">Seguinos en nuestras redes sociales:</p>
+
+<div class="d-flex justify-content-center gap-3 mb-3">
+    <img src="{{ asset('images/brand-whatsapp.svg') }}" width="40">
+    <img src="{{ asset('images/brand-facebook.svg') }}" width="40">
+    <img src="{{ asset('images/brand-instagram.svg') }}" width="40">
+</div>
+
+<p class="text-dark small mb-0">© 2026 - La Dejada Padel - Todos los derechos reservados</p>
+
+</div>
 </nav>
 
 </body>
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -2,38 +2,46 @@
 <html>
 
 <head>
-    <title>Carrito de Compras</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Dejada Padel Store / Carrito de Compras</title>
+    <link rel="icon" type="image/png" href="images/pelota.png">
     <link rel="stylesheet" href="{{ asset('css/estilo.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
 </head>
 
-<body>
-<nav class="navbar navbar-expand-lg navbar-light navbar">
-    <div class="container-fluid py-3">
-            <div class="col-md-3">
-                <a class="navbar-brand d-flex align-items-center" href="/main">
-                    <img src="{{ asset('images/Icono.PNG') }}" alt="Logo La Dejada" width="130" class="me-2">
-                </a>
-            </div>
-            
-            <div class="col-md-6">
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Buscar productos..." aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Buscar</button>
-                </form>
-            </div>
-            
-            <a class="navbar-brand ms-auto" href="/user">
-                <img src="{{ asset('images/user.svg') }}" height="30">
-            </a>
-                    <a class="navbar-brand position-relative" href="/shop">
-    <img src="{{ asset('images/shopping-cart.svg') }}" height="30">
-    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
-</a>
-            </div>
+<body><nav class="navbar navbar-dark navbar">
+    <div class="container-fluid">
+
+    <div class="row w-100 align-items-center">
+
+    <div class="col-12 col-md-3 text-center text-md-start mb-2 mb-md-0">
+        <a href="/main">
+            <img src="{{ asset('images/Icono.PNG') }}" class="img-fluid" style="max-height:60px;">
+        </a>
+    </div>
+
+    <div class="col-12 col-md-6 mb-2 mb-md-0">
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Buscar productos...">
+            <button class="btn btn-outline-light">Buscar</button>
+        </form>
+    </div>
+
+    <div class="col-12 col-md-3 d-flex justify-content-center justify-content-md-end gap-3">
+    
+        <a href="/user">
+            <img src="{{ asset('images/user.svg') }}" height="28">
+        </a>
+
+        <a href="/shop" class="position-relative">
+            <img src="{{ asset('images/shopping-cart.svg') }}" height="28">
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">0</span>
+        </a>
+        </div>
+        </div>
     </div>
 </nav>
-
 
 <div class="container text-center mt-5">
     <h1 class="mb-3">Carrito de Compras</h1>
