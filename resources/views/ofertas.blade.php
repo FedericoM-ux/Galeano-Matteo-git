224baaac053e8a7a1118d4dc29c5db68eccea0b7
Fede <federicoomatteo@gmail.com>
2026-04-29 11:34:58 -0300

    Arreglo en nombres alternativos en los productos

--- resources/views/ofertas.blade.php
+++ resources/views/ofertas.blade.php
@@ -74,7 +74,7 @@
     <div class="row g-4">
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/grip-relieve.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/grip-relieve.jpeg') }}" class="card-img-top p-3" alt="Grip X-Trust">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Grip con relieve X-Trust</p>
                     <p class="text-muted mb-0"><del>$3,000</del></p>
@@ -86,7 +86,7 @@
 
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/remera-head.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/remera-head.jpeg') }}" class="card-img-top p-3" alt="Remera Head">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Remera Head</p>
                     <p class="text-muted mb-0"><del>$25,000</del></p>
@@ -98,7 +98,7 @@
 
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/pelota-penn.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/pelota-penn.jpeg') }}" class="card-img-top p-3" alt="Pelota Penn">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Pelota Penn</p>
                     <p class="text-muted mb-0"><del>$10,000</del></p>
@@ -110,7 +110,7 @@
 
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/pelota-rosa.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/pelota-rosa.jpeg') }}" class="card-img-top p-3" alt="Pelota AmaSport">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Pelota AmaSport</p>
                     <p class="text-muted mb-0"><del>$7,500</del></p>
--- resources/views/ventaMayorista.blade.php
+++ resources/views/ventaMayorista.blade.php
@@ -74,7 +74,7 @@
     <div class="row g-4">
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/caja-bull.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/caja-bull.jpeg') }}" class="card-img-top p-3" alt="Pelotas Bull">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Caja de pelotas Bullpadel</p>
                     <h5 class="fw-bold mb-1">$270,000</h5>
@@ -85,7 +85,7 @@
 
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/caja-penn.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/caja-penn.jpeg') }}" class="card-img-top p-3" alt="Pelotas Penn">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Caja de pelotas Penn</p>
                     <h5 class="fw-bold mb-1">$180,000</h5>
@@ -96,7 +96,7 @@
 
         <div class="col-12 col-md-6 col-lg-3">
             <div class="card h-100 border-0 shadow-sm text-center">
-                <img src="{{ asset('images/caja-xtrust.jpeg') }}" class="card-img-top p-3" alt="Paleta Hombre">
+                <img src="{{ asset('images/caja-xtrust.jpeg') }}" class="card-img-top p-3" alt="Pelotas X-Trust">
                 <div class="card-body">
                     <p class="text-muted small mb-1">Caja de pelotas X-Trust</p>
                     <h5 class="fw-bold mb-1">$200,000</h5>
@@ -117,6 +117,7 @@
         </div>
     </div>
 </div>
+
 <div class="linea-end"></div>
 
 <nav class="navbar navbar-dark navbarW">
